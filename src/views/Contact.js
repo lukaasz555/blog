9acfefed6ac615812ce4520ff3f9e8bafe219b9e
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2022-06-24 16:07:00 +0200

    started /contact

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,16 +1,33 @@
 import React from "react";
 import styled from "styled-components";
+import Input from "components/atoms/Input/Input";
+import { Textarea } from "components/atoms/Textarea/Textarea";
+import BtnReadMore from "components/atoms/BtnReadMore/BtnReadMore";
 
 const Wrapper = styled.div`
   width: 90%;
   margin: 0 auto;
   text-align: center;
+  /*   background-color: rgba(0, 0, 0, 0.15); */
+  /*   display: flex;
+  flex-direction: column; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  flex-wrap: wrap;
 `;
 
 const Contact = () => {
   return (
     <Wrapper>
       <p>coming soon</p>
+
+      <Input placeholder="Your name" />
+      <Input placeholder="Mail address" type="mail" />
+
+      <Textarea placeholder="Message"></Textarea>
+      <BtnReadMore />
     </Wrapper>
   );
 };
