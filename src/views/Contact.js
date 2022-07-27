3d4ed400cf28270c8f42802047a46345557a82c1
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2022-07-27 18:05:19 +0200

    contact and EmailJS

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import Input from "components/atoms/Input/Input";
 import { Textarea } from "components/atoms/Textarea/Textarea";
 import Button from "components/atoms/Button/Button";
+import ContactForm from "components/molecules/ContactForm/ContactForm";
 
 const Wrapper = styled.div`
   width: 90%;
@@ -11,6 +12,7 @@ const Wrapper = styled.div`
   /*   background-color: rgba(0, 0, 0, 0.15); */
   /*   display: flex;
   flex-direction: column; */
+  //background-color: #fab;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -21,11 +23,12 @@ const Wrapper = styled.div`
 const Contact = () => {
   return (
     <Wrapper>
-      <Input placeholder="Your name" />
+      {/*       <Input placeholder="Your name" />
       <Input placeholder="Mail address" type="mail" />
 
       <Textarea placeholder="Message"></Textarea>
-      <Button content={"SEND"} />
+      <Button content={"SEND"} /> */}
+      <ContactForm />
     </Wrapper>
   );
 };
