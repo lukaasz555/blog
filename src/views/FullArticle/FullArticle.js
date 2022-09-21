a161cc8717ceec80007225fcdce92e332f6a83c2
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2022-09-21 12:22:13 +0200

    img loading

--- src/views/FullArticle/FullArticle.js
+++ src/views/FullArticle/FullArticle.js
@@ -1,50 +1,50 @@
-import React from "react";
-import Author from "components/molecules/Author/Author";
-import Button from "components/atoms/Button/Button";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import Author from 'components/molecules/Author/Author';
+import Button from 'components/atoms/Button/Button';
+import { NavLink } from 'react-router-dom';
 import {
-  Wrapper,
-  StyledTop,
-  StyledContent,
-  StyledBottom,
-} from "./FullArticle.styles";
+	Wrapper,
+	StyledTop,
+	StyledContent,
+	StyledBottom,
+} from './FullArticle.styles';
 
 const FullArticle = ({
-  id,
-  title,
-  short,
-  category,
-  content,
-  img = null,
-  date,
-  source,
+	id,
+	title,
+	short,
+	category,
+	content,
+	img = null,
+	date,
+	source,
 }) => (
-  <Wrapper>
-    <StyledTop>
-      <h1>{title}</h1>
-      <p>{date}</p>
-    </StyledTop>
+	<Wrapper>
+		<StyledTop>
+			<h1>{title}</h1>
+			<p>{date}</p>
+		</StyledTop>
 
-    <StyledContent>
-      {img ? <img src={img.url} alt={title} /> : null}
-      <strong>{short}</strong>
-      <p>{content}</p>
-      <span>
-        źródło: <a href={source}>{source}</a>
-      </span>
-    </StyledContent>
-    <StyledBottom>
-      <p>
-        kategoria: <NavLink to="/category">#{category}</NavLink>
-      </p>
+		<StyledContent>
+			{img && <img src={img.url} alt={title} />}
+			<strong>{short}</strong>
+			<p>{content}</p>
+			<span>
+				źródło: <a href={source}>{source}</a>
+			</span>
+		</StyledContent>
+		<StyledBottom>
+			<p>
+				kategoria: <NavLink to='/category'>#{category}</NavLink>
+			</p>
 
-      <NavLink to="/">
-        <Button content="Back" />
-      </NavLink>
-    </StyledBottom>
+			<NavLink to='/'>
+				<Button content='Back' />
+			</NavLink>
+		</StyledBottom>
 
-    <Author />
-  </Wrapper>
+		<Author />
+	</Wrapper>
 );
 
 export default FullArticle;
