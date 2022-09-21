037bffe595cfff950e9889cb6b1943a3783a5469
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2022-09-21 12:13:59 +0200

    lazy loading

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,35 +1,37 @@
-import React, { useEffect, useState, useContext, Provider } from "react";
-import { ThemeProvider } from "styled-components";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { theme } from "assets/theme";
-import GlobalStyle from "assets/styles/GlobalStyles";
-import Header from "components/molecules/Header/Header";
-import MobileNav from "components/molecules/MobileNav/MobileNav";
-import { Wrapper, StyledMain } from "views/Root/Root.styles";
-import About from "../About/About";
-import Contact from "../Contact/Contact";
-import Blog from "../Blog/Blog";
-import Category from "../Category/Category";
-import DesktopNav from "components/molecules/DesktopNav/DesktopNav";
-import FullArticle from "../FullArticle/FullArticle";
-import axios from "axios";
+import React, { useEffect, useState, useContext, Provider } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { theme } from 'assets/theme';
+import GlobalStyle from 'assets/styles/GlobalStyles';
+import Header from 'components/molecules/Header/Header';
+import MobileNav from 'components/molecules/MobileNav/MobileNav';
+import { Wrapper, StyledMain } from 'views/Root/Root.styles';
+import About from '../About/About';
+import Contact from '../Contact/Contact';
+import Blog from '../Blog/Blog';
+import Category from '../Category/Category';
+import DesktopNav from 'components/molecules/DesktopNav/DesktopNav';
+import Loader from 'components/atoms/Loader/Loader';
+//import FullArticle from "../FullArticle/FullArticle";
+import axios from 'axios';
+const LazyFullArt = React.lazy(() => import('../FullArticle/FullArticle'));
 
 export const NavContext = React.createContext({
-  isOpen: false,
-  setOpen: () => {},
-  toggleMobileNav: () => {},
+	isOpen: false,
+	setOpen: () => {},
+	toggleMobileNav: () => {},
 });
 
 const Root = () => {
-  // handle mobile-nav:
-  const [isOpen, setOpen] = useState(false);
-  const toggleMobileNav = () => {
-    setOpen(!isOpen);
-  };
+	// handle mobile-nav:
+	const [isOpen, setOpen] = useState(false);
+	const toggleMobileNav = () => {
+		setOpen(!isOpen);
+	};
 
-  // fetching data from CMS:
-  const URL = "https://graphql.datocms.com/";
-  const query = `
+	// fetching data from CMS:
+	const URL = 'https://graphql.datocms.com/';
+	const query = `
 {
   allArticles {
     id
@@ -47,111 +49,112 @@ const Root = () => {
 }
 `;
 
-  const [articles, setArticles] = useState([]);
-  const [error, setError] = useState("");
-  const [filteredArts, setFilteredArts] = useState([]);
+	const [articles, setArticles] = useState([]);
+	const [error, setError] = useState('');
+	const [filteredArts, setFilteredArts] = useState([]);
 
-  useEffect(() => {
-    axios
-      .post(
-        URL,
-        { query },
-        {
-          headers: {
-            authorization: `Bearer ${process.env.REACT_APP_DATOCMS_TOKEN}`,
-          },
-        }
-      )
-      .then(({ data: { data } }) => {
-        const arts = data.allArticles.map((art) => ({
-          id: +art.id,
-          key: art.id,
-          title: art.title,
-          date: art.date,
-          short: art.short,
-          img: art.img,
-          content: art.content,
-          category: art.category,
-          source: art.source,
-        }));
-        setArticles(arts.sort((a, b) => b.id - a.id));
-      })
-      .catch(() => {
-        setError("Przepraszamy, nie udało się załadować artykułów.");
-      });
-  }, []);
+	useEffect(() => {
+		axios
+			.post(
+				URL,
+				{ query },
+				{
+					headers: {
+						authorization: `Bearer ${process.env.REACT_APP_DATOCMS_TOKEN}`,
+					},
+				}
+			)
+			.then(({ data: { data } }) => {
+				const arts = data.allArticles.map((art) => ({
+					id: +art.id,
+					key: art.id,
+					title: art.title,
+					date: art.date,
+					short: art.short,
+					img: art.img,
+					content: art.content,
+					category: art.category,
+					source: art.source,
+				}));
+				setArticles(arts.sort((a, b) => b.id - a.id));
+			})
+			.catch(() => {
+				setError('Przepraszamy, nie udało się załadować artykułów.');
+			});
+	}, []);
 
-  return (
-    <>
-      <Router>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <Wrapper>
-            <StyledMain>
-              <NavContext.Provider
-                value={{
-                  isOpen,
-                  toggleMobileNav,
-                }}
-              >
-                <Header />
-                <MobileNav />
-                <DesktopNav />
-              </NavContext.Provider>
-              <Routes>
-                <Route
-                  path="/"
-                  element={
-                    <Blog
-                      articles={articles}
-                      setArticles={setArticles}
-                      error={error}
-                      filteredArts={filteredArts}
-                    />
-                  }
-                />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route
-                  path="/category"
-                  element={<Category articles={articles} />}
-                />
-                {articles.map(
-                  ({
-                    id,
-                    title,
-                    short,
-                    category,
-                    content,
-                    img,
-                    date,
-                    source,
-                  }) => (
-                    <Route
-                      key={id}
-                      path={`/${id}`}
-                      element={
-                        <FullArticle
-                          key={id}
-                          title={title}
-                          short={short}
-                          category={category}
-                          content={content}
-                          img={img}
-                          date={date}
-                          source={source}
-                        />
-                      }
-                    />
-                  )
-                )}
-              </Routes>
-            </StyledMain>
-          </Wrapper>
-        </ThemeProvider>
-      </Router>
-    </>
-  );
+	return (
+		<>
+			<Router>
+				<ThemeProvider theme={theme}>
+					<GlobalStyle />
+					<Wrapper>
+						<StyledMain>
+							<NavContext.Provider
+								value={{
+									isOpen,
+									toggleMobileNav,
+								}}>
+								<Header />
+								<MobileNav />
+								<DesktopNav />
+							</NavContext.Provider>
+							<Routes>
+								<Route
+									path='/'
+									element={
+										<Blog
+											articles={articles}
+											setArticles={setArticles}
+											error={error}
+											filteredArts={filteredArts}
+										/>
+									}
+								/>
+								<Route path='/about' element={<About />} />
+								<Route path='/contact' element={<Contact />} />
+								<Route
+									path='/category'
+									element={<Category articles={articles} />}
+								/>
+								{articles.map(
+									({
+										id,
+										title,
+										short,
+										category,
+										content,
+										img,
+										date,
+										source,
+									}) => (
+										<Route
+											key={id}
+											path={`/${id}`}
+											element={
+												<React.Suspense fallback={<Loader />}>
+													<LazyFullArt
+														key={id}
+														title={title}
+														short={short}
+														category={category}
+														content={content}
+														img={img}
+														date={date}
+														source={source}
+													/>
+												</React.Suspense>
+											}
+										/>
+									)
+								)}
+							</Routes>
+						</StyledMain>
+					</Wrapper>
+				</ThemeProvider>
+			</Router>
+		</>
+	);
 };
 
 export default Root;
