52910a330ec173b28698891182e422ed18768114
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2022-09-24 09:52:48 +0200

    deleted async & set instead of filter

--- src/views/Category/Category.js
+++ src/views/Category/Category.js
@@ -1,60 +1,55 @@
-import React, { useState } from "react";
-import Article from "components/organisms/Article/Article";
-import { Wrapper, StyledTop } from "./Category.styles";
-import CategoryButton from "components/atoms/CategoryButton/CategoryButton";
+import React, { useState } from 'react';
+import Article from 'components/organisms/Article/Article';
+import { Wrapper, StyledTop } from './Category.styles';
+import CategoryButton from 'components/atoms/CategoryButton/CategoryButton';
 
 const Category = ({ articles }) => {
-  let categories = [];
+	const getCategories = () => {
+		const categories = new Set(articles.map((item) => item.category));
+		return [...categories];
+	};
 
-  setTimeout(
-    articles.filter(
-      (item) =>
-        !categories.includes(item.category) && categories.push(item.category)
-    ),
-    0
-  );
+	const [filteredCategories, setFilteredCategories] = useState([]);
 
-  const [filteredCategories, setFilteredCategories] = useState([]);
+	const handleFilter = (e) => {
+		if (articles) {
+			const filteredArts = articles.filter(
+				(art) => art.category === e.target.textContent
+			);
+			setFilteredCategories(filteredArts);
+		}
+	};
 
-  const handleFilter = (e) => {
-    if (articles) {
-      const filteredArts = articles.filter(
-        (art) => art.category === e.target.textContent
-      );
-      setFilteredCategories(filteredArts);
-    }
-  };
+	return (
+		<Wrapper>
+			{articles && (
+				<>
+					<StyledTop>
+						<h1>Wybierz kategorię:</h1>
 
-  return (
-    <Wrapper>
-      {articles && (
-        <>
-          <StyledTop>
-            <h1>Wybierz kategorię:</h1>
+						{getCategories().map((item) => (
+							<CategoryButton
+								key={item}
+								itemName={item}
+								onClick={handleFilter}
+							/>
+						))}
+					</StyledTop>
 
-            {categories.map((item) => (
-              <CategoryButton
-                key={item}
-                itemName={item}
-                onClick={handleFilter}
-              />
-            ))}
-          </StyledTop>
-
-          {filteredCategories &&
-            filteredCategories.map((art) => (
-              <Article
-                title={art.title}
-                short={art.short}
-                category={art.category}
-                date={art.date}
-                key={art.id}
-                id={art.id}
-              />
-            ))}
-        </>
-      )}
-    </Wrapper>
-  );
+					{filteredCategories &&
+						filteredCategories.map((art) => (
+							<Article
+								title={art.title}
+								short={art.short}
+								category={art.category}
+								date={art.date}
+								key={art.id}
+								id={art.id}
+							/>
+						))}
+				</>
+			)}
+		</Wrapper>
+	);
 };
 export default Category;
